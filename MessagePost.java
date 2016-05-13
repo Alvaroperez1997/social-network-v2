dbf9ed3fb6929863ff0b880e63f7e0e3c681fbc5
Alvaro <alvaroperbay97@gmail.com>
2016-05-13 12:24:38 +0200

    Creada clase padre para almacenar todos los metodos y atributos iguales

---
+++ MessagePost.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+/**
+ * Write a description of class MessagePost here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MessagePost extends Post
+{
+    private String message;
+
+    /**
+     * Constructor for objects of class MessagePost
+     */
+    public MessagePost(String author, String text)
+    {
+        super(author);
+        message = text;
+    }
+
+    /**
+     * Metodo que devuelve el mensaje del post
+     */
+    public String getText(){
+        return message;
+    }
+}
